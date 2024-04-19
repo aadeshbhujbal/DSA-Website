c5e7fa53029676c045cd078d7c0eecf94c95f38a
Soumyamanna123 <soumyamanna729@gmail.com>
2024-04-19 13:08:50 +0530

    CHANGE

--- src/app/(root)/about-us/page.tsx
+++ src/app/(root)/about-us/page.tsx
@@ -1,9 +1,117 @@
+import { Container } from "@/components/ui/Container"
 import React from "react"
+import Featured from "../(home)/homepagesections/Featured"
+import Button from "@/components/ui/button"
+import Image from "next/image"
 
 
 
-const events = () => {
-  return <div>Blog</div>
+
+
+const AboutUs = () => {
+  return (
+    <div>
+      <div className="bg-black">
+        <div className="px-6 md:px-16 py-10 md:py-24 grid grid-cols-1 md:grid-cols-2">
+          <div className="flex flex-col  gap-3 md:gap-5 ">
+              <p className="h3 text-white">EdTech Pioneers:</p>
+              <p className="text-white h4">Transforming Online Education</p> 
+              <p className="text-white" >We pride ourselves on being the leading EdTech powerhouse in India, dedicated to revolutionizing higher education. Through cutting-edge technology, innovative pedagogy, strategic industry partnerships, and a team of esteemed faculty, we craft unparalleled online learning journeys for students worldwide.</p>    
+              <Button variant={"default"} href="" size="medium" className="mt-2 w-full  max-w-[250px]">
+              Join Our Team{" "}
+              </Button>                 
+          </div>
+          <div className="flex justify-center">
+            <div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    <section>
+      <Container>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-2 gap-y-8 lg:px-10 pb-10">
+          <div className="flex justify-center">
+          <Image
+                src={"/images/our-mission.png"}
+                alt={"our-mission"}
+                width={700}
+                height={400}
+                className="lg:!w-xl lg:max-w-[900px]"
+              />
+          </div>
+          <div className="  flex items-center">
+            <div className="flex flex-col gap-2 md:gap-5 md:pr-4">
+              <p className="h2">Our Mission</p>
+              <p>Our mission is clear: to redefine the landscape of online education through innovative EdTech solutions. We are dedicated to empowering learners globally by providing access to world-class online learning experiences, regardless of geographical or economic barriers. Our goal is to inspire, engage, and support students on their educational journey, equipping them with the skills and knowledge they need to thrive in an ever-evolving world. Through our relentless pursuit of excellence, we aim to shape the future of online learning and make a lasting impact on society.</p>
+            </div>
+            
+          </div>
+        </div>
+        <div className="py-8">
+          <div className="flex justify-center items-center">
+            <p className="h2">Meet Our Top Management Team</p>
+          </div>
+          <div className="flex justify-evenly">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-20 gap-y-8 py-12 ">
+            <div className="flex flex-col gap-2">
+              <div>
+              <Image
+                src={"/images/trainer-profile-picture.png"}
+                alt={"certificate"}
+                width={300}
+                height={400}
+                className="lg:!w-xl lg:max-w-[900px]"
+              />
+              </div>
+              <p className="h4 "style={{ fontWeight: '500' }}>Samiran Santra</p>
+              <p>Co-Founder and CEO</p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <div>
+              <Image
+                src={"/images/trainer-profile-picture.png"}
+                alt={"certificate"}
+                width={300}
+                height={400}
+                className="lg:!w-xl lg:max-w-[900px]"
+              />
+              </div>
+              <p className="h4" style={{ fontWeight: '500' }}>Moutuli Mitra</p>
+              <p>Chief Business Development Officer</p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <div>
+              <Image
+                src={"/images/trainer-profile-picture.png"}
+                alt={"certificate"}
+                width={300}
+                height={400}
+                className="lg:!w-xl lg:max-w-[900px]"
+              />
+              </div>
+              <p className="h4" style={{ fontWeight: '500' }}>Partha Jana</p>
+              <p>Chief Operating Officer</p>
+            </div>
+
+          </div>
+          </div>
+
+
+        </div>
+
+        <div className="flex flex-col gap-4 ">
+          <div className="flex justify-center">
+            <p className="h2">Award And Recognitions</p>
+          </div>
+
+        </div>
+        
+      </Container>
+      <Featured />
+    </section>
+    </div>
+  )
 }
 
-export default events
+export default AboutUs
---
+++ src/app/(root)/refund-policies/page.tsx
@@ -0,0 +1,35 @@
+import { Container } from "@/components/ui/Container"
+import React from "react"
+
+const RefundPolicy = () => {
+  return (
+  <section>
+    <Container>
+      <div className="flex flex-col gap-8 py-8">
+        <p className="h2">Refund  Policy</p>
+        <div className="flex flex-col gap-5">
+          <p>At DataSpace Academy, we are committed to your satisfaction. If you're not completely delighted with your purchase, we offer a hassle-free 30-day money-back guarantee.</p>
+          <p>To initiate a refund, simply reach out to our dedicated customer support team within 30 days of your purchase date. We'll guide you through the process and ensure your refund is processed swiftly.</p>
+          <p>Conditions for a refund:</p>
+          <div className="pl-10">
+            <ul style={{ listStyleType: 'disc' }}>
+              <li>The product/service must be returned in its original condition, undamaged and unused.</li>
+              <li>Refunds are applicable only to purchases made directly through our website.</li>
+              <li>Shipping and handling charges are non-refundable.</li>
+              <li>Please allow up to [X] business days for the refunded amount to reflect in your account.</li>
+            </ul>
+          </div>
+          <p>While we strive to accommodate every refund request, we reserve the right to decline refunds in cases of abuse or misuse of our products/services.</p>
+          <p>Your satisfaction is our priority. Should you have any questions or concerns regarding our refund policy,don't hesitate to contact us at {" "}  <a href={`mailto:${"support@dataspacesecurity.com"}`}>support@dataspacesecurity.com</a></p>
+        </div>
+      </div>
+
+
+    </Container>
+
+  </section>
+
+  )
+}
+
+export default RefundPolicy
